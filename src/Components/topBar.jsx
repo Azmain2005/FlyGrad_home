84300dcd9e06b1fc4b0326b610fc35e43ecec049
Azmain2005 <azmain.2005.mahtab@gmail.com>
2025-08-22 21:18:39 +0600

    first commit

---
+++ src/Components/topBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBar = () => {
+  return (
+    <div className="bg-violet-950 text-white text-center py-1">
+        <marquee className="font-normal sm:text-sm">Ready to get with the times? Get knowledge about abroad.</marquee>
+    </div>
+  );
+};
+
+export default TopBar;
