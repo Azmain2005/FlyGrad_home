a2ee4b3b1952eb2d362bd97644f54517a444aa31
Azmain Mahtab <145844907+Azmain2005@users.noreply.github.com>
2025-10-17 05:44:09 +0600

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FaFacebookF, FaYoutube, FaInstagram } from 'react-icons/fa'; // Import icons
-import text from '../assets/text.svg';
+import text from '../assets/FlyGradLogo.png';
 
 const Footer = () => {
   return (
     <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full bg-gray-900 text-gray-300 mt-30">
         <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
             <div className="md:max-w-96">
-                <img alt="" class="h-11" src={text} />
+                <img alt="" src={text} width={200} />
                 <p className="mt-6 text-sm">
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
@@ -26,17 +26,10 @@ const Footer = () => {
                         <li><a href="#">Privacy policy</a></li>
                     </ul>
                 </div>
-                <div>
-                    <h2 className="font-semibold mb-5">Get in touch</h2>
-                    <div className="text-sm space-y-2">
-                        <p>+1-234-567-890</p>
-                        <p>contact@example.com</p>
-                    </div>
-                </div>
             </div>
         </div>
         <p className="pt-4 text-center text-sm pb-5">
-            Copyright {new Date().getFullYear()} © <a href="https://prebuiltui.com">PrebuiltUI</a>. All Right Reserved.
+            Copyright {new Date().getFullYear()} © <a href="https://flygrad.tech/">flyGrad</a>. All Right Reserved.
         </p>
     </footer>
 )
